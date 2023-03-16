6b81b134406ff563e59ee00819fc2e97361d389c
Huytq <tranquanghuy12110@gmail.com>
2023-03-16 18:07:00 +0700

    init project

---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField] Player _player;
+
+    public void LoadData()
+    {
+
+    }
+}
